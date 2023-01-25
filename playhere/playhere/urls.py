53e3dc591d43499a80bd86fe2907bf9abddff77e
ajayoneness <ajayoneness123@gmail.com>
2023-01-25 14:41:34 +0545

    .............

--- playhere/playhere/urls.py
+++ playhere/playhere/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -17,7 +18,9 @@ urlpatterns = [
     path('contactus/',include('contactus.urls')),
     path('api/',include('api.urls')),
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
