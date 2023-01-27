20f42ce96215125320ec83c76db5b7819c7d254d
ajayoneness <ajayoneness123@gmail.com>
2023-01-27 18:37:45 +0545

    new app download field added

--- playhere/downloadapp/urls.py
+++ playhere/downloadapp/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.downloadApp, name="downloadapp"),
-    path('<str:file_path>/', views.download_file, name='download_file'),
+    path('d/', views.download_file, name='download_file'),
 
 ]
--- playhere/downloadapp/views.py
+++ playhere/downloadapp/views.py
@@ -5,14 +5,14 @@ from django.conf import settings
 from django.http import FileResponse
 
 def downloadApp(request):
+
+    return render(request,'downloadapp.html')
+
+
+
+def download_file(request):
     BASE_DIR = Path(__file__).resolve().parent.parent
-    file_path = os.path.join(BASE_DIR, 'media','app','app-release.apk')
-    print(file_path)
-    return render(request,'downloadapp.html',{'file_path': file_path})
-
-
-
-def download_file(request, file_path):
+    file_path = os.path.join(BASE_DIR, 'media', 'app', 'app-release.apk')
     file = open(file_path, 'rb')
     response = FileResponse(file)
     response['Content-Disposition'] = 'attachment; filename="{}"'.format(file_path)
