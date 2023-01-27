b6b87e5bc26912e7f6a3bada977e639fc1027a97
ajayoneness <ajayoneness123@gmail.com>
2023-01-27 18:05:09 +0545

    new app download field added

---
+++ playhere/downloadapp/views.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import os
+from django.shortcuts import render
+from django.conf import settings
+from django.http import FileResponse
+
+def downloadApp(request):
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    file_path = os.path.join(BASE_DIR, 'media\\app\\app-release.apk')
+    return render(request,'downloadapp.html',{'file_path': file_path})
+
+
+
+def download_file(request, file_path):
+    file = open(file_path, 'rb')
+    response = FileResponse(file)
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(file_path)
+    return response
